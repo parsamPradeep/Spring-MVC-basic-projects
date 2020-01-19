58cb9d12d2bbebc986a9f01ee07043a450335f18
parsamPradeep <parzam77@gmail.com>
2020-01-19 20:05:09 +0530

    addition of spring-security basic'

---
+++ Spring-boot-basic-auth/src/main/java/com/demo/basic/auth/SpringBootBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.demo.basic.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicAuthApplication.class, args);
+	}
+
+}

c05ba3382820fd21349a82b8eb7df01cc5210636
parsamPradeep <parzam77@gmail.com>
2020-04-20 18:00:18 +0530

    addition of two maven modules

---
+++ Spring-pet-clinic/pet-clinic-data/src/main/java/com/spring/petclinic/model/Pet.java
@@ -0,0 +1,28 @@
+package com.spring.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+	private PetType petType;
+	private Owner owner;
+	private LocalDate bday;
+	public PetType getPetType() {
+		return petType;
+	}
+	public void setPetType(PetType petType) {
+		this.petType = petType;
+	}
+	public Owner getOwner() {
+		return owner;
+	}
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+	public LocalDate getBday() {
+		return bday;
+	}
+	public void setBday(LocalDate bday) {
+		this.bday = bday;
+	}
+
+}
